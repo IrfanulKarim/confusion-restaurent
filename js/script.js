2672624fc53f5d7f3e52d7271d3831d2fe46beb4
Irfanul Karim <karimirfanul10@gmail.com>
2020-07-14 12:56:56 +0600

    npm scripts part 1

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({
+        interval: 2000
+    });
+
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children('span').hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children('span').removeClass("fa-pause");
+            $("#carouselButton").children('span').addClass("fa-play");
+        }
+        else{
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children('span').removeClass("fa-play");
+            $("#carouselButton").children('span').addClass("fa-pause");
+        }
+       
+    });
+
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+
+
+    // For tooltip 
+
+    // $(document).ready(function(){
+    //     $('[data-toggle="tooltip"]').tooltip();
+    // });
+
+
+    // Reserve Modal
+
+    $("#reserveModalButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+
+    $("#reserveModalDismiss").click(function(){
+        $("#reserveModal").modal('hide');
+    });
+
+    $("#reserveModalCancel").click(function(){
+        $("#reserveModal").modal('hide');
+    })
+
+
+    // Login Modal
+
+    $("#loginModalButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+
+    $("#loginModalClose").click(function(){
+        $("#loginModal").modal('hide');
+    });
+
+    $("#loginModalCancel").click(function(){
+        $("#loginModal").modal('hide');
+    });
+
+
+
+
+});
